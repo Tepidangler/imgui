e1aa786f12cf90501d2063cf3fb8600aa21d8d21
Tepid Angler <32462088+Tepidangler@users.noreply.github.com>
2025-05-04 09:56:52 -0400

    Added stack layout

--- backends/imgui_impl_dx11.cpp
+++ backends/imgui_impl_dx11.cpp
@@ -647,10 +647,10 @@ static void ImGui_ImplDX11_CreateWindow(ImGuiViewport* viewport)
     sd.SampleDesc.Count = 1;
     sd.SampleDesc.Quality = 0;
     sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
-    sd.BufferCount = 1;
+    sd.BufferCount = 4;
     sd.OutputWindow = hwnd;
     sd.Windowed = TRUE;
-    sd.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+    sd.SwapEffect = DXGI_SWAP_EFFECT_FLIP_SEQUENTIAL;
     sd.Flags = 0;
 
     IM_ASSERT(vd->SwapChain == nullptr && vd->RTView == nullptr);
@@ -716,7 +716,7 @@ static void ImGui_ImplDX11_RenderWindow(ImGuiViewport* viewport, void*)
 static void ImGui_ImplDX11_SwapBuffers(ImGuiViewport* viewport, void*)
 {
     ImGui_ImplDX11_ViewportData* vd = (ImGui_ImplDX11_ViewportData*)viewport->RendererUserData;
-    vd->SwapChain->Present(0, 0); // Present without vsync
+    vd->SwapChain->Present(1, 0); // Present without vsync
 }
 
 static void ImGui_ImplDX11_InitPlatformInterface()
